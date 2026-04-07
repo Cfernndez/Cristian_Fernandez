15318aa7a1e678c7ebf8452dedc2f8a77f5f053c
Cristian <c.fernandezu@duocuc.cl>
2026-04-07 10:28:30 -0400

    crear model

---
+++ src/main/java/com/example/Cristian_Fernandez/model/Recursos.java
@@ -0,0 +1,19 @@
+package com.example.Cristian_Fernandez.model;
+
+import java.time.LocalDate;
+
+public class Recursos {
+
+    
+    
+    private int id;
+    private String nombre;
+    private boolean disponible;
+    private String responsable;
+    private LocalDate fechaCreacion;
+    private String tipo;
+    private int cantidad;
+
+
+
+}
---
+++ src/test/java/com/example/Cristian_Fernandez/CristianFernandezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cristian_Fernandez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristianFernandezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
